eba5b562bbf2a2b331d5ae9f134aea02c1dd1906
sugilog <sugilog@gmail.com>
2019-06-21 14:22:24 +0900

    instant server

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"net/http"
 	"os"
 )
 
 func main() {
-	if dir, err := os.Getwd(); err != nil {
-		panic(err)
-	} else {
-		fmt.Println(dir)
+	flag.Parse()
+	dir := workdir(flag.Args())
+	server := http.FileServer(http.Dir(dir))
+	http.Handle("/", server)
+	http.ListenAndServe(":3000", nil)
+}
+
+func workdir(args []string) string {
+	var dir string
+
+	if len(args) == 1 {
+		if _, err := os.Stat(args[0]); !os.IsNotExist(err) {
+			dir = args[0]
+		}
 	}
+
+	if dir == "" {
+		if d, err := os.Getwd(); err != nil {
+			panic(err)
+		} else {
+			dir = d
+		}
+	}
+
+	return dir
 }
